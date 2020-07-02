4261fff78e5c04392b3a358fc56adc1bdb723443
vanajmoorthy <vanajmoorthy@gmail.com>
2020-07-02 19:09:36 +0530

    s

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./lines.json");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
